2291c61e8bc84595cf1887f9b67627803494fdff
Robert <robert@rmbp.local>
2018-05-30 13:10:00 -0700

    updates

---
+++ js/days.js
@@ -0,0 +1,14 @@
+var daysLeft = function() {
+    var oneDay = 24*60*60*1000;
+    var weddingDate = new Date('Sat May 25 2019 18:00:00 GMT-0700 (PDT)');
+    var today = new Date();
+    return Math.round(Math.abs((today.getTime() - weddingDate.getTime())/(oneDay)));
+};
+
+var holder = document.getElementById('days');
+var textHolder = document.getElementById('daystext');
+
+if (holder && textHolder) {
+  holder.innerText = daysLeft();
+  textHolder.innerText = ' days until the wedding';
+}
